7d567cf2a9f4be2a6e6bd491d641dd78132f2ed7
Sinan G Aksoy <sinanaksoy@Macintosh-001f5bee6747.local>
2014-05-23 16:41:30 -0700

    Adding LLSM vector file

---
+++ llsmvector.m
@@ -0,0 +1,23 @@
+function [ dom ] = llsmvector( A )
+%LLSMVECTOR Summary of this function goes here
+%   Detailed explanation goes here
+
+[n,m]=size(A);
+v=zeros(n,1);
+J=prod(A,2,'native');
+for k=1:n
+    J(k,1)=J(k,1)^(1/5);
+end
+
+sumOfRowProducts=sum(J);
+
+for k=1:n
+    
+   v(k,1)=(J(k,1)/sumOfRowProducts);  
+   
+end
+
+dom=v;
+
+end
+
