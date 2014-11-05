4f0eaa2ce9ffd62715f908a9fbb237ac8bc51afa
Olivia Simpson <oliviamsimpson@gmail.com>
2014-11-05 14:41:41 -0800

    add computation of projection matrices for comparison to skew symmetric mat

--- dominanteigoffensedefense.m
+++ dominanteigoffensedefense.m
@@ -1,8 +1,9 @@
-function [ o, d, r ] = dominanteigoffensedefense( B )
+function [ o, d, r, proj, projY ] = dominanteigoffensedefense( B )
 %Compute the offense/defense ranking vectors and scalar r
 %   B is a skew-symmetric pairwise dominance matrix.  Then if l=ir is the
 %   maximal eigenvalue, let f=a+bi be the corresponding eigenvector.  We
 %   output a=offense ranking, b=defense ranking, r=scalar
+%Also compute our projection matrix and the Youla projection
 
 lambdaB = eig(B);
 lambdaB = abs(lambdaB);
@@ -10,11 +11,23 @@ lambdaB = abs(lambdaB);
 
 [V,L] = eig(B); % V is the matrix whose columns are eigenvectors,
                 % L has corresponding eigenvalues on the diagonal.
-l = L(ind, ind);
+l1 = L(ind, ind);
 r = imag(l);
-f = V(:,ind);
-o = real(f);
-d = imag(f);
+v1 = V(:,ind);
+o = real(v1);
+d = imag(v1);
 
+l2 = conj(l1);
+v2 = conj(v1);
+o2 = real(v2);
+d2 = imag(v2);
+
+% our projection with v1*v1'
+proj = l1*v1*v1' + l2*v2*v2';
+
+% youla projection with fg' - gf'
+projY = imag(l1)*(o*d' - d*o') + imag(l2)*(o2*d2' - d2*o2');
+
+% proj1 == proj2 has negligible error along diagonal
 end
 
